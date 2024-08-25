43ff0ba1c2c28985628712f1252c7509d05de452
vanaigr <vanaigranov@gmail.com>
2024-08-24 21:42:34 -0500

    render all images on one canvas to avoid seams

--- retrieve/compress_backgrounds.py
+++ retrieve/compress_backgrounds.py
@@ -12,11 +12,7 @@ for filename in os.listdir(input_directory):
     input_path = os.path.join(input_directory, filename)
     output_path = os.path.join(output_directory, filename)
 
-    if filename.endswith('.color'):
-        with open(output_path, 'wb') as out_file:
-            pass
-
-    if filename == 'bounds.js':
+    if filename == 'backgrounds.js':
         shutil.copy(input_path, '../' + filename)
 
     if not filename.endswith('.png'):
@@ -28,31 +24,31 @@ for filename in os.listdir(input_directory):
         continue
 
     image = Image.open(input_path)
-    image = image.resize((image.width // 2, image.height // 2), Image.Resampling.HAMMING)
+    # tunrs out you can't have big canvases, have to scale down even more to fit into 16K x 16K max
+    image = image.resize((image.width // 4, image.height // 4), Image.Resampling.HAMMING)
 
     if image.mode == 'RGBA':
         print('  rgba!') # all should be RGB
         image = image.convert('RGB')
 
     imageP = image.convert('P', palette=Image.ADAPTIVE)
+    imageP.save(output_path, format='PNG', optimize=True, compress_level=9)
 
     # stream = io.BytesIO()
     # image.save(stream, format='PNG', optimize=True, compress_level=9)
-
-    streamP = io.BytesIO()
-    imageP.save(streamP, format='PNG', optimize=True, compress_level=9)
-
+    # streamP = io.BytesIO()
+    # imageP.save(streamP, format='PNG', optimize=True, compress_level=9)
     # array = stream.getvalue()
-    arrayP = streamP.getvalue()
+    # arrayP = streamP.getvalue()
     # print('  ' + str(len(array)) + ' vs ' + str(len(arrayP)))
     # if len(array) < len(arrayP):
     #    print('  to rgb')
     #    with open(output_path, 'wb') as out_file:
     #        out_file.write(array)
     # else:
-    #    print('  to palette')
-    with open(output_path, 'wb') as out_file:
-        out_file.write(arrayP)
+    #     print('  to palette')
+    #     with open(output_path, 'wb') as out_file:
+    #         out_file.write(arrayP)
 
     # image.save(output_path, format='PNG', optimize=True, compress_level=9)
 
