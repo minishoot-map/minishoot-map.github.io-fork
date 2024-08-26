9982876d176aab1dc26f9861207fffd7a1cd88b9
vanaigr <vanaigranov@gmail.com>
2024-08-26 16:22:39 -0500

    make reading objects synchronous

--- load.js
+++ load.js
@@ -20,53 +20,39 @@ const shortenName = /[.+](.+)$/
     }
 })()
 
-class Src {
-    constructor(reader) {
-        this.reader = reader
-        this.value = new Uint8Array(0)
-        this.i = 0
-    }
-
-    async peek() {
-        while(this.value && this.i >= this.value.length) {
-            await this._read()
-        }
-        if(!this.value) return null
-        return this.value[this.i];
-    }
-
-    async pop() {
-        const cur = await this.peek()
-        this.skip()
-        return cur
-    }
-
-    skip() {
-        this.i++
-    }
-
-    async _read() {
-        const { done, value } = await this.reader.read()
-        this.i -= this.value.length
-        if(done) this.value = null
-        else this.value = value
-    }
+var src
+function peek() {
+    if(src.i < src.value.length) return src.value[src.i]
+    return src.value[i]
+}
+function pop() {
+    var cur = peek(src)
+    src.i++
+    return cur
+}
+function skip() {
+    src.i++
 }
 
 var scenes
 
 var objectsLoaded = (async() => {
     const response = await fetch('./data/objects.bp')
-    const reader = response.body.getReader()
-    scenes = await parseAny(new Src(reader))
+    const value = new Uint8Array(await response.arrayBuffer())
+    console.log('started')
+    const start = performance.now()
+    src = { i: 0, value }
+    scenes = parseAny()
+    const end = performance.now()
+    console.log('done in ' + (end - start) + 'ms')
     wereObjectsLoaded = true
 })()
 
-async function parseCompressedInt(src) {
+function parseCompressedInt() {
     var res = 0
     var i = 0
     do {
-        var cur = await src.pop()
+        var cur = pop()
         res = res | ((cur & 0b0111_1111) << (i * 7))
         i++
     } while((cur & 0b1000_0000) == 0)
@@ -75,29 +61,29 @@ async function parseCompressedInt(src) {
 
 const bytes4 = new ArrayBuffer(4)
 const bytes4view = new DataView(bytes4)
-async function parseFloat(src) {
-    if(await src.peek() === 0b1111_1111) {
-        src.skip()
+function parseFloat() {
+    if(peek() === 0b1111_1111) {
+        skip()
         return 0
     }
-    for(var i = 3; i > -1; i--) bytes4view.setUint8(i, await src.pop())
+    for(var i = 3; i > -1; i--) bytes4view.setUint8(i, pop())
     return bytes4view.getFloat32(0, true)
 }
-async function parseVector2(src) {
-    if(await src.peek() === 0b0111_1111) {
-        src.skip()
+function parseVector2() {
+    if(peek() === 0b0111_1111) {
+        skip()
         return [0, 0]
     }
-    const x = await parseFloat(src)
-    const y = await parseFloat(src)
+    const x = parseFloat()
+    const y = parseFloat()
     return [x, y]
 }
 
-async function parseString(src) {
+function parseString() {
     var res = ''
-    if(await src.peek() == 0b1000_0000) return res
+    if(peek() == 0b1000_0000) return res
     do {
-        var cur = await src.pop()
+        var cur = pop()
         res += String.fromCharCode(cur & 0b0111_1111)
     } while((cur & 0b1000_0000) == 0)
     return res
@@ -105,7 +91,7 @@ async function parseString(src) {
 
 const primParsers = {
     ["GameManager+None"]: () => { throw "None is not parsable" },
-    ["System.Boolean"]: async(src) => await src.pop() != 0,
+    ["System.Boolean"]: () => pop() != 0,
     ["System.Int32"]: parseCompressedInt,
     ["System.Single"]: parseFloat,
     ["System.String"]: parseString,
@@ -114,33 +100,37 @@ const primParsers = {
     ["UnityEngine.Vector2"]: parseVector2,
     ["GameManager+Any"]: parseAny,
 }
-
-async function parsePrimitive(schemaI, src) {
-    return await primParsers[schemas[schemaI].name](src)
+const primIParsers = Array(10)
+for(const key in primParsers) {
+    primIParsers[typeSchemaI[key]] = primParsers[key]
 }
 
-async function parseAny(src) {
-    var schemaI = await parseCompressedInt(src)
+function parsePrimitive(schemaI) {
+    return primIParsers[schemaI]()
+}
+
+function parseAny() {
+    var schemaI = parseCompressedInt()
     var schema = schemas[schemaI]
-    return await parseBySchema(schemaI, src)
+    return parseBySchema(schemaI)
 }
 
-async function parseBySchema(schemaI, src) {
+function parseBySchema(schemaI) {
     const schema = schemas[schemaI]
     const type = schema.type
     if(type === 0) {
-        return await parsePrimitive(schemaI, src)
+        return parsePrimitive(schemaI)
     }
     else if(type === 1) {
-        return await parseRecord(schemaI, src)
+        return parseRecord(schemaI)
     }
     else if(type === 2) {
-        return await parseArray(schemaI, src)
+        return parseArray(schemaI)
     }
     else throw ("No type " + type)
 }
 
-async function parseRecord(schemaI, src) {
+function parseRecord(schemaI) {
     const schema = schemas[schemaI]
 
     const names = schema.members
@@ -148,22 +138,22 @@ async function parseRecord(schemaI, src) {
 
     const res = {}
     for(var i = 0; i < names.length; i++) {
-        res[names[i]] = await parseBySchema(types[i], src)
+        res[names[i]] = parseBySchema(types[i])
     }
     if(schema.base != null) {
-        res._base = await parseBySchema(schema.base, src)
+        res._base = parseBySchema(schema.base)
     }
     res._schema = schemaI
 
     return res
 }
 
-async function parseArray(schemaI, src) {
+function parseArray(schemaI) {
     const schema = schemas[schemaI]
-    const len = await parseCompressedInt(src)
+    const len = parseCompressedInt()
     const res = Array(len)
     for(var i = 0; i < len; i++) {
-        res[i] = await parseBySchema(schema.elementT, src)
+        res[i] = parseBySchema(schema.elementT)
     }
     res._schema = schemaI
     return res
