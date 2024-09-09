93cd68bea0598ed4f9dcf0fc4d93beccfa4bd41c
vanaigr <vanaigranov@gmail.com>
2024-09-09 15:41:51 -0500

    change pallete algorithm to median cut

--- src/process/compress_backgrounds.js
+++ src/process/compress_backgrounds.js
@@ -47,138 +47,156 @@ counts[bgInt] = (counts[bgInt] ?? 0) + 100/*arbitrary*/
 const resizedPixels = await Promise.all(residedPixelsP)
 console.log('counted pixels')
 
+// radix sort bucket counts for each channel
+const bucketSize = 256
+const channelValuesCounts = new Uint32Array(bucketSize * 3)
+const startOffsets = new Uint32Array(bucketSize)
+
+const rounds = 8
+
+const buckets = new Uint32Array(2 ** rounds)
+var bucketI = 0
+
+function palletize(remSpits, beginI, endI, colors, colorsDst) {
+    if(beginI == endI) {
+        console.warn('lost a palette color')
+        return
+    }
+    if(remSpits <= 0) {
+        buckets[bucketI++] = endI
+        return
+    }
+
+    channelValuesCounts.fill(0)
+
+    var rmin = 255, rmax = 0
+    var gmin = 255, gmax = 0
+    var bmin = 255, bmax = 0
+
+    for(let i = beginI; i < endI; i++) {
+        const r = colors[i*3    ]
+        channelValuesCounts[r]++
+        rmin = Math.min(rmin, r)
+        rmax = Math.max(rmax, r)
+
+        const g = colors[i*3 + 1]
+        channelValuesCounts[bucketSize + g]++
+        gmin = Math.min(gmin, g)
+        gmax = Math.max(gmax, g)
+
+        const b = colors[i*3 + 2]
+        channelValuesCounts[bucketSize * 2 + b]++
+        bmin = Math.min(bmin, b)
+        bmax = Math.max(bmax, b)
+    }
+
+    var off = 0
+    var diff = rmax - rmin
+    if(gmax - gmin > diff) {
+        off = 1
+        diff = gmax - gmin
+    }
+    if(bmax - bmin > diff) {
+        off = 2
+        diff = bmax - bmin
+    }
+
+    // console.log('chose', off, diff)
+
+    var sum  = 0
+    for(let i = 0; i < bucketSize; i++) {
+        startOffsets[i] = (beginI + sum) * 3
+        sum += channelValuesCounts[off * bucketSize + i]
+    }
+
+    var totalPixelsCount = 0
+    for(let i = beginI; i < endI; i++) {
+        const r = colors[i*3    ]
+        const g = colors[i*3 + 1]
+        const b = colors[i*3 + 2]
+
+        const v = colors[i * 3 + off]
+        const offset = startOffsets[v]
+
+        startOffsets[v] += 3
+        colorsDst[offset    ] = r
+        colorsDst[offset + 1] = g
+        colorsDst[offset + 2] = b
+
+        const color = r | (g << 8) | (b << 16)
+        totalPixelsCount += counts[color]
+    }
+
+    const median = totalPixelsCount * 0.5
+
+    var prevC = 0, curC = 0
+    var countI = beginI
+    for(; countI < endI; countI++) {
+        const color = colorsDst[countI*3] | (colorsDst[countI*3 + 1] << 8) | (colorsDst[countI*3 + 2] << 16)
+        prevC = curC
+        curC += counts[color]
+        if(curC >= median) break
+    }
+
+    if(median - prevC < curC - median) countI = countI - 1
+    countI = Math.min(Math.max(beginI + 1, countI), endI - 1)
+
+    // console.log('   ', beginI, countI, endI, curC, prevC, median)
+    palletize(remSpits - 1, beginI, countI, colorsDst, colors)
+    palletize(remSpits - 1, countI, endI, colorsDst, colors)
+}
+
+
 const uniqueColors = Object.keys(counts)
 const colorsC = uniqueColors.length
 
-const centroidC = 256
-
-if(colorsC < centroidC) throw new Error('Not enough colors')
-function genCentroids() {
-    random.use(52)
-    const centroids = new Uint32Array(centroidC)
-
-    const taken = {}
-    for (let i = 0; i < centroidC; i++) {
-        do {
-            var ri = uniqueColors[random.int(0, colorsC -1)]
-        } while(taken[ri] != null)
-        taken[ri] = 0
-        centroids[i] = ri
-    }
-
-    return centroids
-}
-
-const centroids = genCentroids()
-
-const countsA = new Uint32Array(colorsC * 2)
+const colors = new Uint8Array(colorsC * 3)
 for(let i = 0; i < colorsC; i++) {
     const c = uniqueColors[i]
-    countsA[i*2    ] = c
-    countsA[i*2 + 1] = counts[c]
+    colors[i*3    ] = (c      ) & 0xff
+    colors[i*3 + 1] = (c >>  8) & 0xff
+    colors[i*3 + 2] = (c >> 16) & 0xff
 }
+const colors2 = new Uint8Array(colorsC * 3)
 
-function iterate() {
-    var totalDifferences = new Float32Array(centroidC * 3)
-    var totalCounts = new Uint32Array(centroidC)
+palletize(rounds, 0, colorsC, colors, colors2)
+const resultColors = rounds & 1 ? colors2 : colors
 
-    for(var i = 0; i < countsA.length; i += 2) {
-        var count = countsA[i*2 + 1]
-
-        var col = countsA[i*2]
-        var r = (col      ) & 0xff
-        var g = (col >>  8) & 0xff
-        var b = (col >> 16) & 0xff
-
-        var minDist = 1 / 0
-        var minJ = -1
-
-        for(var j = 0; j < centroidC; j++) {
-            var ccol = centroids[j]
-            var dr = r - ((ccol      ) & 0xff)
-            var dg = g - ((ccol >>  8) & 0xff)
-            var db = b - ((ccol >> 16) & 0xff)
-            var dist = dr*dr + dg*dg + db*db
-            if(dist < minDist) {
-                minJ = j
-                minDist = dist
-            }
-        }
-
-        {
-            var ccol = centroids[minJ]
-            var dr = r - ((ccol      ) & 0xff)
-            var dg = g - ((ccol >>  8) & 0xff)
-            var db = b - ((ccol >> 16) & 0xff)
-
-            totalDifferences[minJ*3    ] += dr * count
-            totalDifferences[minJ*3 + 1] += dg * count
-            totalDifferences[minJ*3 + 2] += db * count
-            totalCounts[minJ] += count
-        }
-    }
-
-    for(var i = 0; i < centroidC; i++) {
-        var ccol = centroids[i]
-        var cr = (ccol      ) & 0xff
-        var cg = (ccol >>  8) & 0xff
-        var cb = (ccol >> 16) & 0xff
-
-        // console.log(totalDifferences[i*3], totalDifferences[i*3 + 1], totalDifferences[i*3 + 2], totalCounts[i])
-
-        if(totalCounts[i] == 0) continue
-        var tic = 1 / totalCounts[i]
-        var r = Math.min(Math.max(0, cr + Math.round(totalDifferences[i*3    ] * tic)), 255)
-        var g = Math.min(Math.max(0, cg + Math.round(totalDifferences[i*3 + 1] * tic)), 255)
-        var b = Math.min(Math.max(0, cb + Math.round(totalDifferences[i*3 + 2] * tic)), 255)
-
-        centroids[i] = r | (g << 8) | (b << 16)
-    }
-}
-
-for(let iter = 0; iter < 10; iter++) {
-    console.log('iteration', iter)
-    iterate()
-}
-
-// output centroids
-/*
-if(centroidC != 256) console.warn('skipping centroids image')
-else {
-    const buffer = Buffer.alloc(centroids.length * 4)
-    centroids.forEach((value, index) => {
-        buffer.writeUInt32LE(value, index * 4)
-        buffer.writeUint8(255, index * 4 + 3)
-    })
-    sharp(buffer, { raw: { width: 16, height: 16, channels: 4 } })
-        .png()
-        .toFile(join(dstPath, 'image' + iter + '.png'))
-}
-*/
+console.log('done')
 
 const palette = {}
-for(let i = 0; i < uniqueColors.length; i++) {
-    const col = uniqueColors[i]
-    const r = (col      ) & 0xff
-    const g = (col >>  8) & 0xff
-    const b = (col >> 16) & 0xff
+let prevOff = 0
+for(let i = 0; i < bucketI; i++) {
+    const curOff = buckets[i]
+    var tr = 0, tg = 0, tb = 0, total = 0
+    for(let j = prevOff; j < curOff; j++) {
+        const r = resultColors[j*3    ]
+        const g = resultColors[j*3 + 1]
+        const b = resultColors[j*3 + 2]
 
-    let minDist = 1 / 0
-    let minJ = -1
+        const color = r | (g << 8) | (b << 16)
+        const count = counts[color]
 
-    for(let j = 0; j < centroidC; j++) {
-        const ccol = centroids[j]
-        const dr = r - ((ccol      ) & 0xff)
-        const dg = g - ((ccol >>  8) & 0xff)
-        const db = b - ((ccol >> 16) & 0xff)
-        const dist = dr*dr + dg*dg + db*db
-        if(dist < minDist) {
-            minJ = j
-            minDist = dist
-        }
+        tr += (r - 127) * count
+        tg += (g - 127) * count
+        tb += (b - 127) * count
+        total += count
     }
 
-    palette[col] = centroids[minJ]
+    const ar = Math.min(Math.max(0, Math.round(tr / total + 127)), 255)
+    const ag = Math.min(Math.max(0, Math.round(tg / total + 127)), 255)
+    const ab = Math.min(Math.max(0, Math.round(tb / total + 127)), 255)
+    const avg = ar | (ag << 8) | (ab << 16)
+
+    for(let j = prevOff; j < curOff; j++) {
+        const r = resultColors[j * 3]
+        const g = resultColors[j*3 + 1]
+        const b = resultColors[j*3 + 2]
+        const color = r | (g << 8) | (b << 16)
+        palette[color] = avg
+    }
+
+    prevOff = curOff
 }
 
 console.log('generating output')
